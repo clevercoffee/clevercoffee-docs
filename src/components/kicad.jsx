5caed15362f115d85a6f53458fbe7bbceab894d8
THOMAS KROEBER - SF03900 <thomas.kroeber@stellantis.com>
2025-01-25 22:10:19 +0100

    add basepath to kicad import

--- src/components/kicad.jsx
+++ src/components/kicad.jsx
@@ -1,17 +1,21 @@
-import Script from "next/script";
+"use client";
 
-export default function KiCAD({
+import Script from "next/script";
+import { addBasePath } from 'next/dist/client/add-base-path';
+export default async function KiCAD({
   src,
   controls = "none",
   width = "100%",
   height = "600px"
 }) {
+  // If src is a local path (starts with "/"), prepend the basePath.
+  // If src is an external URL (e.g., "https://..."), leave it as-is.
   return (
     <>
       {/* Load the KiCanvas script from the CDN */}
       <Script
         id="kicanvas-script"
-        src="/kicanvas.js"
+        src={addBasePath("/kicanvas.js")}
         type="module"
       />
 
