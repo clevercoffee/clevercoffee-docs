f4dd750d79bb3d24d0609a345fe55173fb7ce54f
THOMAS KROEBER - SF03900 <thomas.kroeber@stellantis.com>
2025-01-27 17:53:12 +0100

    upgrade, introduce carousel component in sebis guide

--- src/mdx-components.ts
+++ src/mdx-components.ts
@@ -3,6 +3,7 @@ import { Pre, withIcons } from "nextra/components";
 import { GitHubIcon } from "nextra/icons";
 import { Caution } from "@/components/caution";
 import KiCAD from "@/components/kicad";
+import Carousel from "./components/carousel";
 
 const docsComponents = getDocsMDXComponents({
   pre: withIcons(Pre, { js: GitHubIcon }),
@@ -12,5 +13,6 @@ export const useMDXComponents: typeof getDocsMDXComponents = (components) => ({
   ...docsComponents,
   ...components,
   Caution,
-  KiCAD
+  KiCAD,
+  Carousel
 });
